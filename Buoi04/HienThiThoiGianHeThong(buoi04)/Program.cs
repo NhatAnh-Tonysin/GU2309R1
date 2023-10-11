4b0493487bef8ffbb15d69e75e8fb820c5620220
Kid <nhatanh.designer@gmail.com>
2023-10-11 22:41:45 +0700

    systemtime(buoi04)

---
+++ Buoi04/HienThiThoiGianHeThong(buoi04)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HienThiThoiGianHeThong_buoi04_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Xin chào");
+
+            DateTime Thoigian = DateTime.Now;
+            Console.WriteLine("Thời gian hiện tại là: " + Thoigian);
+
+
+            Console.ReadKey();
+        }
+    }
+}
