17388950156ae1eed1e6974b223a90297e1b6087
Kid <nhatanh.designer@gmail.com>
2023-10-11 22:55:03 +0700

    TinhDienTichHCN(buoi04)

---
+++ Buoi04/ThuchanhToanTu(Buoi04)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThuchanhToanTu_Buoi04_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Viết một ứng dụng để tính diện tích của hình chữ nhật dựa vào chiều rộng và chiều cao được nhập vào.
+            
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Write("Nhập thông số chiều dài: ");
+            float dai = float.Parse(Console.ReadLine());
+            Console.Write("Nhập thông số chiều rộng: ");
+            float rong = float.Parse(Console.ReadLine());
+
+            float dientich = dai * rong;
+            Console.WriteLine("Diện tích hình chữ nhật là: " + dai + "x" + rong + "=" + dientich);
+;
+            Console.ReadKey();
+
+        }
+    }
+}
