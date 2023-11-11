ab6164b5bb4fee2532df8198bf380862774d6652
Kid <nhatanh.designer@gmail.com>
2023-11-11 17:50:08 +0700

    THClass

---
+++ Buoi17/AnimalClass/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClass
+{
+    public abstract class Animal
+    {
+        protected string CanNang { get; set; }
+        protected string ChieuCao { get; set; }
+
+        public Animal(string cannang, string chieucao)
+        {
+            this.CanNang = cannang;
+            this.ChieuCao = chieucao;
+        }
+        public abstract void PrintInfo();
+    }
+
+    class Cat : Animal
+    {
+        private string Ten { get; set; }
+        public Cat(string cannang, string chieucao, string ten) : base(cannang, chieucao)
+        {
+            this.Ten = ten;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("CanNang: {0}\nChieuCao: {1}\nTen: {2}", this.CanNang, this.ChieuCao, this.Ten);
+        }
+
+    }
+}
---
+++ Buoi17/AnimalClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "Kitty");
+            cat.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
